b7f1e31f294dc7096a451da74004edf1bedc05e8
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-09-09 05:31:25 +0530

    added cors to backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
 const gameUserRoutes = require("./routes/userRoutes");
 const gameRoutes = require("./routes/gameRoutes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 3000;
+const port = 8000;
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
