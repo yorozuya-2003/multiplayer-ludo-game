9fba8ab710c2d303145e7aa3aad70e39d09aed7e
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-09-10 02:45:13 +0530

    added morgan package to track api requests - also explored possibility of including dice-value in game-state response from the player-turn table

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const gameUserRoutes = require("./routes/userRoutes");
 const gameRoutes = require("./routes/gameRoutes");
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 const port = 8000;
 
