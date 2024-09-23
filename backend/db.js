d6cf4c97e19b07a0ae3ac01ae95b671569fcae98
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-09-23 15:00:23 +0530

    added postgres database url connection string

--- backend/db.js
+++ backend/db.js
@@ -5,20 +5,10 @@ dotenv.config();
 const Pool = require("pg").Pool;
 
 // database configuration
-const postgresUser = process.env.POSTGRES_USER;
-const postgresPassword = process.env.POSTGRES_PASSWORD;
-const postgresDb = process.env.POSTGRES_DB;
-const postgresPort = 5432;
-const postgresHost = "localhost";
-
-// const dbConnectionString = `postgres://${postgresUser}:${postgresPassword}@${postgresHost}:${postgresPort}/${postgresDb}`;
+const connectionString = process.env.POSTGRES_URL;
 
 const pool = new Pool({
-  user: postgresUser,
-  password: postgresPassword,
-  host: postgresHost,
-  port: postgresPort,
-  database: postgresDb,
+  connectionString
 });
 
 module.exports = pool;
