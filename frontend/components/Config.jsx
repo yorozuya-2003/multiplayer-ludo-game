31788a00f8500424117a324f32002548459e8ae7
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-09-23 16:14:32 +0530

    added backend api url to environment variables

--- frontend/components/Config.jsx
+++ frontend/components/Config.jsx
@@ -1,3 +1,7 @@
-const API_URL = 'http://localhost:8000';
+// loading environment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+const API_URL = process.env.BACKEND_API_URL;
 
 export default API_URL;
