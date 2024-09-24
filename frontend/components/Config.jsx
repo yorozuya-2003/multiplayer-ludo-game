f9a711299fdf5ce5639f446c512c6d7d68c55ef3
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-09-24 10:46:28 +0530

    checking for api-url

--- frontend/components/Config.jsx
+++ frontend/components/Config.jsx
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
 
-const API_URL = `${apiDomain}`;
+// const API_URL = `${apiDomain}`;
+const API_URL = `https://ludo-tanish-backend.up.railway.app`;
 
 export default API_URL;
